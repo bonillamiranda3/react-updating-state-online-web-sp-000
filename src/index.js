f028e517895a7aa8f8c733360e4aee0fa51bb8d7
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-10 06:49:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
- 
+
 ReactDOM.render(<ClickityClick />, document.getElementById('root'));
 ReactDOM.render(
   <div>
